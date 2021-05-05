536fef963ac23baf53558c859e631eb22e6e6ce3
Danilo Balarini <danilo.balarini@gmail.com>
2021-05-04 23:50:34 -0300

    initial commit

---
+++ src/test/kotlin/br/com/dblogic/cartolareports/CartolaReportsApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.dblogic.cartolareports
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CartolaReportsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
