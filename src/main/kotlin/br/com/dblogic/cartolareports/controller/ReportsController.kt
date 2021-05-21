207455fc54e129303d21db88c7c23868c119a2ee
Danilo Balarini <danilo.balarini@gmail.com>
2021-05-21 19:40:49 -0300

    git services available and working 100%

--- src/main/kotlin/br/com/dblogic/cartolareports/controller/ReportsController.kt
+++ src/main/kotlin/br/com/dblogic/cartolareports/controller/ReportsController.kt
@@ -1,9 +1,14 @@
 package br.com.dblogic.cartolareports.controller
 
+import br.com.dblogic.cartolareports.service.GitService
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import java.nio.file.Path
+import java.nio.file.Paths
 
 @Controller
 @RequestMapping("")
@@ -11,9 +16,36 @@ class ReportsController {
 
     private val logger = LoggerFactory.getLogger(ReportsController::class.java)
 
+    @Value("\${github.cartola}")
+    lateinit var caRtola: String
+
+    @Autowired
+    lateinit var gitService: GitService
+
     @GetMapping("/hello")
     fun helloworld() : String {
         logger.info("opa")
         return "helloworld"
     }
+
+    @GetMapping("/pwd")
+    fun pwd(): String {
+        val currentWorkingDir: Path = Paths.get("").toAbsolutePath()
+        logger.info("currentWorkingDir: $currentWorkingDir")
+        return "helloworld"
+    }
+
+    @GetMapping("/pull")
+    fun gitPull(): String {
+        logger.info("PULL!")
+        val currentWorkingDir: Path = Paths.get("").toAbsolutePath()
+        val resources = Paths.get("$currentWorkingDir/src/main/resources").toAbsolutePath()
+        val caRtola = Paths.get("$resources/$caRtola").toAbsolutePath()
+
+        logger.info("fazendo o pull no diretório $caRtola")
+        gitService.gitPull(caRtola)
+
+        return "helloworld"
+    }
+
 }
